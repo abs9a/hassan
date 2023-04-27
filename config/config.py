94b33259e5b79d4f42dddfd21e078058267ee7d9
abs2a <131925760+abs2a@users.noreply.github.com>
2023-04-27 14:27:16 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -14,7 +14,7 @@ API_ID = int(getenv("API_ID", "12987601"))
 API_HASH = getenv("API_HASH","a6581bb9160931c45197461bb7e81f90")
 
 ## توكن البوت بنجيبو من هنا @Botfather in Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN","1480911289:AAF-CN5yyxF3Dmhws9ZFWy3xlFmyCTMgzyI")
+BOT_TOKEN = getenv("BOT_TOKEN","1480911289:AAHFn14TpnylOLe3F04CYQdsiVKHgySGmKk")
 
 # المونج لو ممعكش سيبو زي مهوه
 MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://moon:bootsmoon@cluster0.uyvnnox.mongodb.net/?retryWrites=true&w=majority")
